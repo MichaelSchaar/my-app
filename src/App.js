5ac0735c9058f011ea36cbcdbaccbd617c8a00c3
Michael Schaar <mschaar@ucsd.edu>
2024-04-30 14:30:15 -0700

    yaya

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import profileImage from './johnny.png';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={profileImage} alt="Michael Schaar" />
         <h1>Hi there, I'm Michael Schaar.</h1> {/* Larger text for greeting */}
         <p>I am a web developer!</p>
         <a
